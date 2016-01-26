49700ba1e394e74b1d94c76cee26a68aaee290f7
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-01-26 17:11:54 +0000

    Update dpx_calc.py

--- dpx_calc.py
+++ dpx_calc.py
@@ -2,19 +2,17 @@ from easygui import multenterbox
 msg = "Fill out these things please"
 title = "blablablabl"
 fieldNames = ["Width",
-	          "Height",
-			  "Bit Depth", 
+	      "Height",
+	      "Bit Depth", 
               "FPS", 
-			  "minutes"]
+              "minutes"]
 fieldValues = []  # we start with blanks for the values
 fieldValues = multenterbox(msg,title, fieldNames)
-print fieldValues
 
-if fieldValues[2] == '10':
-	bitdepth      = '32'
+if fieldValues[2]   == '10':
+	bitdepth    =  '32'
 elif fieldValues[2] == '16':
-	bitdepth = '48'
-print fieldValues
- 	
+	bitdepth     = '48'
+
 evaluate = float(fieldValues[0]) * float(fieldValues[1])  * float(bitdepth) //8 // 1024 // 1024 * float(fieldValues[3]) * 60 * float(fieldValues[4]) //1024 
 print "%s minutes of footage will take up %s gigabytes of space" % (fieldValues[4], evaluate)
