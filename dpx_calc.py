3c0f38008de95f669d7bce7e13e783dcb5934c80
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-01-26 17:22:32 +0000

    add comments

--- dpx_calc.py
+++ dpx_calc.py
@@ -10,9 +10,18 @@ fieldValues = []  # we start with blanks for the values
 fieldValues = multenterbox(msg,title, fieldNames)
 
 if fieldValues[2]   == '10':
-	bitdepth    =  '32'
+	bitdepth    =  '32'       # While 10 bits * 3 components = 30 bits, they are stored as 32 bits.
 elif fieldValues[2] == '16':
-	bitdepth     = '48'
+	bitdepth     = '48'       # 16 bits * 3 components(RGB) = 48 bits
 
-evaluate = float(fieldValues[0]) * float(fieldValues[1])  * float(bitdepth) //8 // 1024 // 1024 * float(fieldValues[3]) * 60 * float(fieldValues[4]) //1024 
-print "%s minutes of footage will take up %s gigabytes of space" % (fieldValues[4], evaluate)
+evaluate = (float(fieldValues[0]) # gets width
+ * float(fieldValues[1])	  # multiples by height
+ * float(bitdepth)                # multiplies by bit depth per component
+ //8 // 1024 // 1024 		  # these calcs turn the value from a bit into a megabyte	
+ * float(fieldValues[3])          # multiply by FPS
+ * 60                             # multiply by 60 to get how many megs for one second
+ * float(fieldValues[4])          # multiply by how many minutes
+ //1024 )                         # turn the value into gigabytes
+ 
+ # prints the result to the screen
+ print "%s minutes of footage will take up %s gigabytes of space" % (fieldValues[4], evaluate)
